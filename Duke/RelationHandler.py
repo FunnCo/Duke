085560fb62202d98a24d24023aa0e1ed04635e54
FunnCo <mareev.maksimka@gmail.com>
2022-05-17 16:49:02 +0300

    RelationHandler for task 1 created

---
+++ Duke/RelationHandler.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+
+class Features(Enum):
+    REFLECTIVE = 1
+    SYMMETRIC = 2
+    TRANSITIVE = 3
+
+
+class BinaryRelation:
+
+    graph = None
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def checkTransitive(self):
+        isTransitive = False
+
+        # Проверка на транзитивность
+        for rowIndex in range(0, len(self.graph)):
+            for columnIndex in range(0, len(self.graph)):
+                currentValue = self.graph[rowIndex][columnIndex]
+                if(currentValue == 1 and rowIndex != columnIndex):
+                    for secondIndex in range(0, len(self.graph)):
+                        secondValue = self.graph[columnIndex][secondIndex]
+                        if(secondValue == 1 and secondIndex != columnIndex):
+                            if(self.graph[rowIndex][secondIndex] == 1):
+                                isTransitive = True
+                            elif(isTransitive):
+                                return False
+
+        return isTransitive
+
+    def checkForFeatures(self):
+        result = []
+
+        # Проверка на рефлексивность
+        isReflective = True
+        for row in range(0, len(self.graph)):
+            if(self.graph[row][row] == 0):
+                isReflective = False
+                break
+
+        if(isReflective):
+            result.append(Features.REFLECTIVE)
+
+        # Проверка на симметрию и ассиметрию
+        isSymmetric = True
+
+        for rowIndex in range(0, len(self.graph)):
+            for columnIndex in range(0, rowIndex + 1):
+
+                currentValue = self.graph[rowIndex][columnIndex]
+                mirroredValue = self.graph[columnIndex][rowIndex]
+
+               
+                if(currentValue + mirroredValue != 0):
+                    if(currentValue + mirroredValue != 2):
+                        isSymmetric = False
+
+        if(isSymmetric):
+            result.append(Features.SYMMETRIC)
+
+        if(self.checkTransitive() == True):
+            result.append(Features.TRANSITIVE)
+
+        return result
+
+    def getAdditionalRelation(self):
+        additionalRelation = []
+
+        for rowIndex in range(0, len(self.graph)):
+            additionalRelation.append([])
+            for columnIndex in range(0, len(self.graph)):
+                additionalRelation[rowIndex].append(
+                    abs(self.graph[rowIndex][columnIndex]-1))
+
+        return additionalRelation
+
+    def getInversedRelation(self):
+        inversedRelation = []
+
+        for rowIndex in range(0, len(self.graph)):
+            inversedRelation.append([])
+            for columnIndex in range(0, len(self.graph)):
+                inversedRelation[rowIndex].append(
+                    self.graph[columnIndex][rowIndex])
+
+        return inversedRelation
